006c9d1a3bd1a4c83de612e37f81d0e3b4c389a9
RijutaRajbhandari <rijuta.191429@ncit.edu.np>
2024-02-16 20:56:32 +0545

    TRIAL

---
+++ detection.py
@@ -0,0 +1,19 @@
+#load modules
+
+#load video
+
+#read frame
+
+#detect car
+
+#track car
+
+#detect lisence plate to car
+
+#crop lisence plate
+
+#process lisence plate
+
+#read lisence plate number
+
+#write results
