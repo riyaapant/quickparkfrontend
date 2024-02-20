4179142817b9a37733cd9ba8a911751f07c451c2
RijutaRajbhandari <rijuta.191429@ncit.edu.np>
2024-02-20 20:45:17 +0545

    Real time license plate detection implementation

--- detection.py
+++ detection.py
@@ -1,6 +1,7 @@
-#from ultralytics import YOLO
-#from ultralytics.models.yolo.detect.predict import DetectionPredictor
-#import cv2
+from ultralytics import YOLO
+from ultralytics.models.yolo.detect.predict import DetectionPredictor
+import cv2
 
-#model=YOLO("E:/Hello/dataset/best.pt")
-#model.predict(source="0", show=True, conf=0.5)
+model=YOLO("C:/Users/user/Boeing_I3C/quickpark/detection.pt")
+model.predict(source="0", show=True, conf=0.5)
+
