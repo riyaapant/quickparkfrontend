167d2ed8f0306732bde2ebfb13f7bb3fb4e408b2
sagarsapkota <sagar.191336@ncit.edu.np>
2024-05-10 19:16:13 +0545

    Parking model creation

--- backend/parking/models.py
+++ backend/parking/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+UserModel = get_user_model()
+
+class parking_location(models.Model):
+    user = models.ForeignKey(UserModel,on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    fee = models.DecimalField(max_digits=5,decimal_places=2,default=80.0)
+    total_spot = models.IntegerField()
