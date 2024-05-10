815110f20be2e949adc14959b5af1445eebb7dbb
sagarsapkota <sagar.191336@ncit.edu.np>
2024-05-10 19:27:47 +0545

    Reservation model created

--- backend/parking/models.py
+++ backend/parking/models.py
@@ -8,3 +8,14 @@ class parking_location(models.Model):
     address = models.CharField(max_length=255)
     fee = models.DecimalField(max_digits=5,decimal_places=2,default=80.0)
     total_spot = models.IntegerField()
+    used_spot = models.IntegerField(default=0)
+    lat = model.DecimalField(max_digits=8,decimal_places=6)
+    lon = model.DecimalField(max_digits=8,decimal_places=6)
+
+class reservation(models.Model):
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    parking = models.ForeignKey(parking_location, on_delete=model.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(null=True)
+    total_amount = models.DecimalField(max_digits=8,decimal_places=2,null=True)
+    
