b905ed42f6a1e29673f921ffe64acfe04cd75b56
sagarsapkota <sagar.191336@ncit.edu.np>
2024-05-10 19:32:28 +0545

    Models updated

--- backend/parking/models.py
+++ backend/parking/models.py
@@ -9,13 +9,13 @@ class parking_location(models.Model):
     fee = models.DecimalField(max_digits=5,decimal_places=2,default=80.0)
     total_spot = models.IntegerField()
     used_spot = models.IntegerField(default=0)
-    lat = model.DecimalField(max_digits=8,decimal_places=6)
-    lon = model.DecimalField(max_digits=8,decimal_places=6)
+    lat = models.DecimalField(max_digits=8,decimal_places=6)
+    lon = models.DecimalField(max_digits=8,decimal_places=6)
 
 class reservation(models.Model):
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    parking = models.ForeignKey(parking_location, on_delete=model.CASCADE)
+    parking = models.ForeignKey(parking_location, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField(null=True)
     total_amount = models.DecimalField(max_digits=8,decimal_places=2,null=True)
-    
+
