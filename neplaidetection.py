289744a326657babdc8e865b0d664354ab88560c
RijutaRajbhandari <rijuta.191429@ncit.edu.np>
2024-05-10 14:21:46 +0545

    Front and back plate detction model

--- neplaidetection.py
+++ neplaidetection.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 from ultralytics.models.yolo.detect.predict import DetectionPredictor
 import cv2
 
-model=YOLO("C:/Users/user/Boeing_I3C/quickpark/best.pt")
-model.predict(source="0", conf=0.5, show=True)
+model=YOLO("C:/Users/user/Boeing_I3C/quickpark/vehicles.pt")
+model.predict(source="C:/Users/user/Boeing_I3C/quickpark/MOVIE.mp4", conf=0.2, save=True)
