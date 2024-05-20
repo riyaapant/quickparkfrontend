ce5828a5def2d50a16d8b80ccc5e3150e0c28400
Riya Pant <riya.191333@ncit.edu.np>
2024-05-20 14:42:47 +0545

    interactive map with user location fetch

--- frontend/src/maps/maps.jsx
+++ frontend/src/maps/maps.jsx
@@ -1,37 +1,105 @@
 import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Maps2 = () => {
 
-    const apikey = import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY
+  const apikey = import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY
 
-    const [userLocation, setUserLocation] = useState({
-        lat: 27.6715029,
-        lng: 85.3332635,
-    });
+  const [locationFetched, setLocationFetched] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('');
+  const [userLocation, setUserLocation] = useState(null);
+  const [message, setMessage] = useState(true)
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            setUserLocation({
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            })
-          },
-          (error) => {
-            console.error("Error getting location", error);
-            // setUserLocation(defaultCenter);
-          }
+  const fetchUserLocation = () => {
+    console.log("Getting user location...");
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setUserLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+        setLocationFetched(true)
+        setMessage(true)
+        setErrorMessage('')
+        console.log(userLocation)
+        // console.log("User location fetched")
+      },
+      (error) => {
+        // console.error("Error getting location", error);
+        setErrorMessage("Error getting location. Please try again.")
+      }
+    );
+  };
+
+  const onMapClick = (e) => {
+    setUserLocation({
+      lat: e.detail.latLng.lat,
+      lng: e.detail.latLng.lng,
+    })
+    console.log(userLocation)
+  }
+
+  const onMarkerDrag = (e) => {
+    const newLocation = {
+      lat: e.latLng.lat(),
+      lng: e.latLng.lng()
+    };
+    setUserLocation(newLocation);
+    console.log(userLocation)
+  }
+
+  return (
+    <main className='flex flex-col justify-center items-center h-screen'>
+
+      {!locationFetched ?
+        (
+          <>
+            <button
+              className="w-48 rounded-md bg-indigo-600 py-1.5 text-md font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={fetchUserLocation}
+            >
+              Get your location
+            </button>
+            {errorMessage && (
+              <p className='mt-4 text-red-600'>{errorMessage}</p>
+            )}
+          </>
         )
-      }, [])
 
-    return (
-        <APIProvider apiKey={apikey}>
-            <Map center={userLocation} zoom={13}>
-                <Marker position={userLocation} />
-            </Map>
-        </APIProvider>
-    )
+        :
+
+        (
+          <>
+            <APIProvider
+              apiKey={apikey}>
+
+              <Map
+                id='get-user-location'
+                center={userLocation}
+                defaultZoom={15}
+                onClick={onMapClick}
+              >
+                <Marker
+                  position={userLocation}
+                  draggable={true}
+                  onDragEnd={onMarkerDrag}
+                />
+              </Map>
+            </APIProvider >
+            {message &&
+              <>
+                <p className='text-lg'>Click on the map or drag the marker to change your location. Click  
+                 <Link to="/dashboard/home" className='text-qp underline'>confirm</Link> once you're done.</p>
+              </>
+            }
+          </>
+        )
+
+      }
+
+    </main>
+  )
 }
 
 export default Maps2
